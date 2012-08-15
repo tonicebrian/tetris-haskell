c5e1c28cab0ee4d31e44f77c26528048f4f4784b
Toni Cebrián <ancechu@gmail.com>
2012-08-16 00:13:25 +0200

    First experiments with Gtk2Hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,18 @@
-main = undefined
+module Main(Main.main) where
+
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Gdk.GC
+
+main :: IO()
+main = do
+    let bluishGray = Color 48 99 99
+        bluishSilver = Color 210 255 255
+    initGUI
+    dia <- dialogNew
+    contain <- dialogGetUpper dia
+    canvas <- drawingAreaNew
+    canvas `on` sizeRequest $ return (Requisition 700 400)
+
+    widgetShowAll dia
+    mainGUI
+    
