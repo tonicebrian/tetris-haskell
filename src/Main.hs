3952fccd2b9701441fdb0d707e7ff3002dbb2e79
Toni Cebrián <ancechu@gmail.com>
2012-08-17 17:30:32 +0200

    More fighting with events in Gtk2Hs

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ import Graphics.UI.Gtk.Gdk.GC
 import Graphics.Rendering.Cairo
 
 import Control.Monad.State as MS
+import Control.Concurrent.MVar
+
 import AbstractUI
 
 -- Just for reference
@@ -17,12 +19,17 @@ keyDown  = 65364
 keyLeft  = 65361
 keyRight = 65363
 
+type ViewState = String
+type State = MVar ViewState
+
 main :: IO()
 main = do
     let bluishGray = Color (256*48) (256*99) (256*99)
         bluishSilver = Color (256*210) (256*255) (256*255)
         
     initGUI
+
+    ui <- newMVar "left" 
 
     -- GUI components
     window <- windowNew
@@ -34,19 +41,46 @@ main = do
     containerAdd frame canvas
     widgetModifyBg canvas StateNormal bluishGray
 
-
     -- Events and callbacks
     window `onDestroy` mainQuit
 
     -- Show and run
     widgetShowAll window
+
     drawin <- widgetGetDrawWindow canvas
-    window `on` keyPressEvent $ tryEvent hKeyPress
+
+    -- TODO. Functions onXXX are deprecated see what is the proper way to do
+    -- this using the pattern window `on` keyPressedEvent (exposeEvent)
+    onExpose canvas $ \_ -> do 
+                                st <- readMVar ui
+                                putStrLn st
+                                myPaint st drawin
+
+
+    --window `on` keyPressEvent $ hKeyPress ui >>= liftIO . putStrLn . eventTime >> return True
+    onKeyPress window $ \_ -> hKeyPress ui >> return True
 
     mainGUI
     
+
+keyPressHandler ui drawin = do
+    hKeyPress ui
+
+renderScene ui drawin = do 
+    st <- readMVar ui
+    putStrLn st
+    myPaint st drawin
+
+ 
+myPaint :: DrawableClass a => ViewState -> a -> IO Bool
+myPaint st drawin = renderWithDrawable drawin (do
+                                                   myDraw st
+                                                   return True
+                                                )
+
 -- Handles all the keyboard interactions
-hKeyPress = do
+hKeyPress :: MVar ViewState -> EventM EKey ()
+hKeyPress mvs = do
    let print = liftIO . putStrLn . keyName 
    key <- eventKeyVal
    case key of
@@ -56,17 +90,12 @@ hKeyPress = do
      65361  -> print key
      65363  -> print key
 
-myDraw :: Double -> Double -> Render()
-myDraw w h  = do
-    setSourceRGB 1 1 1
-    paint 
 
-    setSourceRGB 210 255 255 
-    setLineWidth 5
-
-    moveTo (0.5 * w) (0.43 * h)
-    lineTo (0.33 * w) (0.71 * h)
-    lineTo (0.66 * w) (0.71 * h)
-    closePath
-
+myDraw :: String -> Render()
+myDraw message = do
+    setSourceRGB 0 0 0
+    moveTo 100 100
+    showText message
     stroke
+
+        
